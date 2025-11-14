1492d5e0ce118f5231c1065a2b25e43ad9fc8c15
andres <andreshurtado3000@icloud.com>
2025-11-14 13:31:49 -0500

    complemento historia 1

--- semana2.py
+++ semana2.py
@@ -8,9 +8,10 @@
 # - Eliminar productos
 # - Buscar productos por código
 
-
+# el codigo gira en un diccionario y listas dentro del diccionario, entonces inicializamos uno vacio
 productos={}
-
+#en cada def creamos una funcion, es un bloque de codigo que podria usar en otras partes
+# En este mostramos un menu con todas sus opciones
 def mostrar_menu():
     print("======menu principal=======")
     print("1. ver productos ")
@@ -22,10 +23,12 @@ def mostrar_menu():
 
 # funcion para ver productos
 def mostrar_productos():
+    #esta condicion es contraria, significa que si el diccionario esta vacio o iguala 0 entonces muestre
     if not productos:
         print("============================")
         print("no hay productos para mostra")
         print("============================")
+        #si si hay elementos muestrelos
     else:
         print("=====Inventario=====")
         #recorremos los datos para mostrar el inventario
@@ -33,8 +36,9 @@ def mostrar_productos():
             print(F" codigo  -  nombre  -  precio  -  cantidad")
             print(F"{codigo} - {nombre} - {precio} - {cantidad}")
 
-#Mostra productos
+#registrar productos
 def registrar_productos():
+    #usamos el try que es un manejo de errore, para poder capturarlos si es el caso
     try:
         codigo=int(input("Ingrese el codigo del producto: "))
         if codigo in productos:
@@ -43,26 +47,32 @@ def registrar_productos():
             print("======================")
             return      
           #salimos si el producto existe
+          # si no existe ingresamos los valores del nuevo producto
         nombre=input("Ingrese el nombre del producto: ")
         precio=input("Ingrese el precio del producto: ")
         cantidad=input("Ingrese la cantidad del producto: ")
-
+        #agregamos
         productos[codigo]=(nombre, precio, cantidad)
         print(f"{nombre} registrado con exito")
+        #capturamos el error
     except ValueError:
         print("ingrese un numero valido, intente nuevamente")
-
+# funcion eliminar producto
 def eliminar_producto():
+    #try para capturar errores
     try: 
+        #le pedimos codigo del producto al usuario
         codigo = int(input("ingrese el codigo del producto a eliminar"))
         if codigo in productos:
+            #usamos el .pop para eliminar un producto en especifico
             eliminado = productos.pop(codigo)
             print("el producto fue eliminado del inventario")
         else:
             print("el producto no existe")
+    # capturamos el error
     except ValueError:
         print("ingrese un numero codigo valido")
-
+#definimos la funcion actualizar producto
 def actualizar_producto():
     try:
         codigo = int(input("ingrese el codigo del producto a actualizar"))
@@ -75,13 +85,14 @@ def actualizar_producto():
             print(f"{nombre} actualizado con exito")
 
         else:
-            print("producto no existe")
-
+            print("")
+    # capturamos el error
     except ValueError:
         print("ingrese un numero valido")
- 
+#Definimos la funcion buscar productos
 def buscar_producto():
     try:
+        # pedimos datos al usuario, busacmos y mostramos
         codigo=int(input("Ingrese el codigo del producto que deseas buscar"))
         if codigo in productos:
             nombre, precio, cantidad = productos[codigo]
